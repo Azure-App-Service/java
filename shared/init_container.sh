1484de74a1de1d33e61469b438effcefb05c9c1f
Shrirang Shirodkar <shrirs@microsoft.com>
2019-03-12 16:55:37 -0700

    Propagate all environment variables to other sessions (ssh, etc)
    - Currently, we only propagate a specific list of environment variables to the interactive sessions (ssh)
    - This commit relaxes this behavior by propagating all environment variables to /etc/profile
    - We now add variables to /etc/profile instead of ~/.profile to make it available not just to the interactive sessions, but also non-interactive sessions.

--- shared/init_container.sh
+++ shared/init_container.sh
@@ -28,55 +28,11 @@ then
     export WEBSITE_INSTANCE_ID=dev
 fi
 
-# After all env vars are defined, add the ones of interest to ~/.profile
-# Adding to ~/.profile makes the env vars available to new login sessions (ssh) of the same user.
+# BEGIN: Configure /etc/profile
 
-# list of variables that will be added to ~/.profile
-export_vars=()
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
-# Step 1. Add app settings to ~/.profile
-# To check if an environment variable xyz is an app setting, we check if APPSETTING_xyz is defined as an env var
-while read -r var
-do
-    if [ -n "`printenv APPSETTING_$var`" ]
-    then
-        export_vars+=($var)
-    fi
-done <<< `printenv | cut -d "=" -f 1 | grep -v ^APPSETTING_`
-
-# Step 2. Add well known environment variables to ~/.profile
-well_known_env_vars=( 
-    HTTP_LOGGING_ENABLED
-    WEBSITE_SITE_NAME
-    WEBSITE_ROLE_INSTANCE_ID
-    JAVA_OPTS
-    JAVA_HOME
-    JAVA_VERSION
-    WEBSITE_INSTANCE_ID
-    _JAVA_OPTIONS
-    CLASSPATH
-    )
-for var in "${well_known_env_vars[@]}"
-do
-    if [ -n "`printenv $var`" ]
-    then
-        export_vars+=($var)
-    fi
-done
-
-# Step 3. Add environment variables with well known prefixes to ~/.profile
-while read -r var
-do
-    export_vars+=($var)
-done <<< `printenv | cut -d "=" -f 1 | grep -E "^(WEBSITE|APPSETTING|SQLCONNSTR|MYSQLCONNSTR|SQLAZURECONNSTR|CUSTOMCONNSTR)_"`
-
-# Write the variables to be exported to ~/.profile
-for export_var in "${export_vars[@]}"
-do
-    echo Exporting env var $export_var
-    # We use single quotes to preserve escape characters
-	echo export $export_var=\'`printenv $export_var`\' >> ~/.profile
-done
+# END: Configure /etc/profile
 
 # If a Start Up file is specified (in the Application Settings blade), it will be passed as an argument.
 if [ ! -z "$1" ]
