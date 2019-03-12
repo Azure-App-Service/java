18a958e09c43b028edafc253827a07a34b6a86b6
Shrirang Shirodkar <shrirs@microsoft.com>
2019-03-12 11:54:23 -0700

    Update setup.sh
    - Added missing steps to copy files to the java11-alpine directory

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,8 @@
 rm -rf jre8-alpine/tmp/shared
 mkdir -p jre8-alpine/tmp
 cp -r shared jre8-alpine/tmp/shared
+
+rm -rf java11-alpine/tmp/shared
+mkdir -p java11-alpine/tmp
+cp -r shared java11-alpine/tmp/shared
+
